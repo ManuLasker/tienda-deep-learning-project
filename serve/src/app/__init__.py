b3dc8942ea5a82a8d533ea1fad6d71bc3786e107
ManuLasker <emanuel.afanador@koombea.com>
2021-05-18 18:24:41 -0500

    first commit for init files

--- serve/src/app/__init__.py
+++ serve/src/app/__init__.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from app.config.variables import DEBUG
+# from app.dl_model.models import BBPredictor, ClassifierPredictor
 
+# # load predictors before creating the app
+# BBPredictor.load_model()
+# ClassifierPredictor.load_model()
+
+# Crate app
 app = FastAPI(debug=DEBUG)
 
 from app import routes
