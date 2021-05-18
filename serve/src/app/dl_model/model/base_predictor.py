c60d0bff451e15490205171406b7c94de868daf7
ManuLasker <emanuel.afanador@koombea.com>
2021-05-18 18:23:21 -0500

    feat: add abstract method as a base for all the predictors classes

---
+++ serve/src/app/dl_model/model/base_predictor.py
@@ -0,0 +1,22 @@
+import torch
+from abc import abstractmethod, ABC
+
+class BasePredictor(ABC):
+    """Predictor Base Class for deep learning models
+    """
+    model:torch.nn.Module = None
+    model_path:str = None
+    
+    @classmethod
+    def load_model(cls):
+        if cls.model is None:
+            cls.model = torch.jit.load(cls.model_path)
+    
+    @classmethod
+    def set_model_path(cls, path:str):
+        if cls.model_path is None:
+            cls.model_path = path
+            
+    @abstractmethod
+    def predict(self, image):
+        ...
