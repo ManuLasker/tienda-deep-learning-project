b95f733e0bbad33e24894d713138ded8804bf1ca
ManuLasker <emanuel.afanador@koombea.com>
2021-05-20 15:32:10 -0500

    feat: update routes models for request and responses

--- serve/src/app/route_model/models.py
+++ serve/src/app/route_model/models.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from typing import Tuple, List
 
+
 class DetectedProduct(BaseModel):
     """Detected product model, this contains information
     about the detected product such as product name and the bounding box
@@ -13,24 +14,28 @@ class DetectedProduct(BaseModel):
         bounding_box: [x0, y0, x1, y1]
     }
     """
+
     product_id: int
     product_name: str
     detection_index: int
     confidence: float
     bounding_box: Tuple[int, int, int, int]
 
+
 class InvocationRequest(BaseModel):
     """Invocation Request for /invocations endpoint,
     This must contain an image in base64 string format encoded as follow
     {
-        image: str
+        base64_image: str
     }
     """
-    image: str # input image
+
+    base64_image: str  # input image
+
 
 class InvocationResponse(BaseModel):
     """Invocation Response for /invocations endpoint,
-    this will respond an image in base64 string format encoded, and 
+    this will respond an image in base64 string format encoded, and
     the detected products information as follows:
     {
         total_classes: int
@@ -45,5 +50,34 @@ class InvocationResponse(BaseModel):
         ]
     }
     """
+
     total_classes: int
-    detected_products: List[DetectedProduct]    
+    detected_products: List[DetectedProduct]
+
+
+class ModelInfo(BaseModel):
+    """Model loading status information.
+    {
+        YoloV5Predictor_loading_Status: bool,
+        ClassifierPredictor_loading_status: bool
+    }
+    """
+
+    YoloV5Predictor_loading_Status: bool
+    ClassifierPredictor_loading_status: bool
+
+
+class PingResponse(BaseModel):
+    """ping response for /ping endpoint.
+    this will respond status model or raise an exception.
+    {
+        message: str,
+        models_info: {
+            YoloV5Predictor_loading_Status: bool,
+            ClassifierPredictor_loading_status: bool
+        }
+    }
+    """
+
+    message: str
+    models_info: ModelInfo
