9df23817219cb600b3b8456b606c254aa53a84bb
ManuLasker <emanuel.afanador@koombea.com>
2021-05-20 15:33:30 -0500

    feat: add base64_to_pil utility and load_image_from_base64_image utility

--- serve/src/app/dl_model/utils.py
+++ serve/src/app/dl_model/utils.py
@@ -2,22 +2,25 @@ import torch
 import torchvision.transforms as T
 import numpy as np
 import cv2
-from typing import Any, Dict, Tuple, Union
+from typing import Tuple, Union
 from base64 import b64decode, b64encode
 from io import BytesIO
 from PIL import Image
 
+
 class ImageUtilities:
     # std and mean configuration
     mean, std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
-    
+
     @staticmethod
-    def rectangular_resize_with_pad(img: np.ndarray,
-                                    new_shape: Union[Tuple[int, int], int] = (640, 640),
-                                    color: Union[Tuple[int, int, int], int] = (114, 114, 114),
-                                    auto: bool = True, 
-                                    scaleFill: bool = False,
-                                    scaleup: bool = True):
+    def rectangular_resize_with_pad(
+        img: np.ndarray,
+        new_shape: Union[Tuple[int, int], int] = (640, 640),
+        color: Union[Tuple[int, int, int], int] = (114, 114, 114),
+        auto: bool = True,
+        scaleFill: bool = False,
+        scaleup: bool = True,
+    ):
         """Resize with pad a numpy array image in BGR format to a specified size,
         but this resize is rectangular that means it will keep one dimension;
         but the other will shrink to fit some rectangular ratio.
@@ -31,44 +34,54 @@ class ImageUtilities:
             scaleFill (bool, optional): Stretch or not. Defaults to False.
             scaleup (bool, optional): scale up or only scale down. Defaults to True.
         """
-        shape = img.shape[:2] # current shape [height, width]
-        
+        shape = img.shape[:2]  # current shape [height, width]
+
         # validate new_shape and color types
         if isinstance(new_shape, int):
             new_shape = (new_shape, new_shape)
-        
+
         if isinstance(color, int):
             color = (color, color, color)
-            
+
         # scale ratio (new / old)
-        r = min(new_shape[0]/shape[0], new_shape[1]/shape[1])
-        if not scaleup: # only scale down, do not scale up
+        r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+        if not scaleup:  # only scale down, do not scale up
             r = min(r, 1.0)
         # compute padding
-        ratio = r, r # width, height ratios
-        new_unpad = (int(round(shape[1] * r)),
-                     int(round(shape[0] * r))) # width, height
-        dw, dh = (new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]) # wh padding
-        if auto: # minimum rectangle
-            dw, dh = np.mod(dw, 32), np.mod(dh, 32) # wh padding
-        elif scaleFill: # stretch image
+        ratio = r, r  # width, height ratios
+        new_unpad = (
+            int(round(shape[1] * r)),
+            int(round(shape[0] * r)),
+        )  # width, height
+        dw, dh = (
+            new_shape[1] - new_unpad[0],
+            new_shape[0] - new_unpad[1],
+        )  # wh padding
+        if auto:  # minimum rectangle
+            dw, dh = np.mod(dw, 32), np.mod(dh, 32)  # wh padding
+        elif scaleFill:  # stretch image
             dw, dh = 0.0, 0.0
             new_unpad = (new_shape[1], new_shape[0])
-            ratio = new_shape[1] / shape[1], new_shape[0] / shape[0] # width, height ratios
-        
-        dw /= 2 # divide padding by 2
-        dh /= 2 # divide
-        
-        if shape[::-1] != new_unpad: # resize
+            ratio = (
+                new_shape[1] / shape[1],
+                new_shape[0] / shape[0],
+            )  # width, height ratios
+
+        dw /= 2  # divide padding by 2
+        dh /= 2  # divide
+
+        if shape[::-1] != new_unpad:  # resize
             img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
-            
+
         top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
         left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-        img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
-        return img        
-    
+        img = cv2.copyMakeBorder(
+            img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color
+        )
+        return img
+
     @staticmethod
-    def _base64_to_pil(base64_image:str) -> Image.Image:
+    def _base64_to_pil(base64_image: str) -> Image.Image:
         """Convert base64 image string to PIL image
         Args:
             base64Image (str): base 64 str image decode
@@ -82,7 +95,7 @@ class ImageUtilities:
         if pil_image.mode == "RGBA":
             pil_image = pil_image.convert("RGB")
         return pil_image
-    
+
     @staticmethod
     def _pil_to_base64(pil_image: Image.Image) -> str:
         """Convert pil image to base64 encode string format
@@ -98,7 +111,7 @@ class ImageUtilities:
             pil_image.save(_buffer, format="PNG")
         img_str = b64encode(_buffer.getvalue()).decode("utf-8")
         return img_str
-    
+
     @staticmethod
     def _pil_to_numpy(image: Image.Image, _format: str = "RGB") -> np.ndarray:
         """Convert image to numpy array image of specific format from oposite format.
@@ -111,8 +124,8 @@ class ImageUtilities:
             np.ndarray: numpy image array.
         """
         image = np.array(image)
-        return ImageUtilities._transform_image(image)
-        
+        return ImageUtilities._transform_image(image, _format)
+
     @staticmethod
     def _transform_image(image: np.ndarray, _format: str = "RGB") -> np.ndarray:
         """Convert image numpy array from BGR to RGB and RGB to BGR.
@@ -128,7 +141,7 @@ class ImageUtilities:
             return cv2.cvtColor(image, code=cv2.COLOR_RGB2BGR)
         elif _format == "BGR":
             return cv2.cvtColor(image, code=cv2.COLOR_BGR2RGB)
-        
+
     @staticmethod
     def _load_image_from_file(file_path: str) -> np.ndarray:
         """load image from file in BGR format.
@@ -140,7 +153,7 @@ class ImageUtilities:
             np.ndarray: numpy image array in BGR format.
         """
         return cv2.imread(file_path)
-    
+
     @staticmethod
     def _to_tensor(image: Union[Image.Image, np.ndarray]) -> torch.Tensor:
         """Convert pil Image or numpy image to tensor Image, if numpy image is np.uint8
@@ -151,7 +164,7 @@ class ImageUtilities:
             torch.Tensor: tensor image.
         """
         return T.ToTensor()(image)
-    
+
     @staticmethod
     def _to_pil(image: Union[torch.Tensor, np.ndarray]) -> Image.Image:
         """Convert tensor or numpy Image to PIL image format, numpy image needs to be
@@ -161,17 +174,53 @@ class ImageUtilities:
         Returns:
             Image.Image: PIL image format
         """
-        if isinstance(image, torch.Tensor): return T.ToPILImage()(image)
-        elif isinstance(image, np.ndarray): return Image.fromarray(image)
-        
+        if isinstance(image, torch.Tensor):
+            return T.ToPILImage()(image)
+        elif isinstance(image, np.ndarray):
+            return Image.fromarray(image)
+
     @staticmethod
-    def process_fastai_model(image: Union[np.ndarray, Image.Image],
-                             new_shape: Tuple[int, int]) -> torch.Tensor: 
-        transform = T.Compose([
-            T.ToTensor(),
-            T.Resize(size=new_shape),
-            T.Normalize(mean=ImageUtilities.mean,
-                        std=ImageUtilities.std)
-        ]) 
-        image:torch.Tensor = transform(image)
+    def process_fastai_model(
+        image: Union[np.ndarray, Image.Image], new_shape: Tuple[int, int]
+    ) -> torch.Tensor:
+        transform = T.Compose(
+            [
+                T.ToTensor(),
+                T.Resize(size=new_shape),
+                T.Normalize(mean=ImageUtilities.mean, std=ImageUtilities.std),
+            ]
+        )
+        image: torch.Tensor = transform(image)
         return image.float()
+
+    @staticmethod
+    def _base64_to_pil(base64_image: str) -> Image.Image:
+        """Convert base64 encoded string image to PIL Image.
+
+        Args:
+            base64_image (str): base 64 str image encode
+
+        Returns:
+            Image.Image: Pil image
+        """
+        # Select just the image information if ther is more than one
+        if len(base64_image.split(",")) > 1:
+            _, base64_image = base64_image.split(",")
+        pil_image = Image.open(BytesIO(b64decode(base64_image)))
+        if pil_image.mode == "RGBA":
+            pil_image.convert("RGB")
+        return pil_image
+
+    @staticmethod
+    def _load_image_from_base64_image(base64_image: str) -> np.ndarray:
+        """load image from  base64 encoded string image to numpy image in BGR format.
+
+        Args:
+            base64_image (str): base 64 str image encode
+
+        Returns:
+            np.ndarray: numpy image array in BGR format.
+        """
+        return ImageUtilities._pil_to_numpy(
+            ImageUtilities._base64_to_pil(base64_image), _format="BGR"
+        )
