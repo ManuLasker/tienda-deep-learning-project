79839c770376a0f2e932e83caa07d931e7fb0e43
ManuLasker <emanuel.afanador@koombea.com>
2021-05-20 15:35:23 -0500

    feat: add health_check_models method dependency to add to ping and invocations routes

---
+++ serve/src/app/dl_model/__init__.py
@@ -0,0 +1,43 @@
+from typing import Generator, Tuple
+from app.dl_model.model.predictor import YoloV5Predictor, ClassifierPredictor
+from app.dl_model import YoloV5Predictor, ClassifierPredictor
+from app.error import ModelLoadingError
+
+# Method for telling the app that models where loaded
+def health_check_models() -> Generator[None, Tuple[bool, bool], None]:
+    """Health check for models loaded to app.
+
+    Raises:
+        ModelLoadingError: raise model error loading if error catches
+        
+    Yields:
+        [Generator[bool, bool]]: generator indicating if model where loaded.
+    """
+    try:
+        # setup predictors before creating the app
+        YoloV5Predictor.setup_model(
+            model_path="/Users/koombea/Desktop/emanuel"
+            "/tienda-deep-learning-project/serve/models/224_mvp.pt",
+            class_names=["items"],
+            anchors = [
+                [10,13, 16,30, 33,23], # P3/8
+                [30,61, 62,45, 59,119], # P4/16
+                [116,90, 156,198, 373,326] # P5/32
+            ]
+        )
+
+        ClassifierPredictor.setup_model(
+            model_path="/Users/koombea/Desktop/emanuel"
+            "/tienda-deep-learning-project/serve/models/mobilenetv3.pt",
+            class_names=["Arroz Doble Vitamor Diana x 500 g",
+                        "CocaCola x 250 ml",
+                        "Maracuya",
+                        "Chicharrón Americano Jacks x 15 g",
+                        "CocaCola x 400 ml",
+                        "Papas de limón 39gr"],
+            product_external_ids=[29856, 30978, 31742, 32057, 30981, 32191]
+        )
+        yield (YoloV5Predictor.model is not None,
+               ClassifierPredictor.model is not None)
+    except Exception as exception:
+        raise ModelLoadingError(detail=str(exception))
