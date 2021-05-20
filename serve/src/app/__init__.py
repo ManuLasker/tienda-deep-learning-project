929cd93b44f74e8ac0c33ea3a2e980e71c41c2f6
ManuLasker <emanuel.afanador@koombea.com>
2021-05-20 15:36:04 -0500

    feat: complete fastapi application for yolo server

--- serve/src/app/__init__.py
+++ serve/src/app/__init__.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI
 from app.config.variables import DEBUG
-# from app.dl_model.models import BBPredictor, ClassifierPredictor
-
-# # load predictors before creating the app
-# BBPredictor.load_model()
-# ClassifierPredictor.load_model()
 
 # Crate app
-app = FastAPI(debug=DEBUG)
+app = FastAPI(debug=DEBUG,
+            title="Tienda App Inference Server",
+            description="Tienda Inference Server for detection products and classification")
 
 from app import routes
