c3ffa217c057dcaacf3efceeb6ac5abb3672a19b
ManuLasker <emanuel.afanador@koombea.com>
2021-05-20 15:31:45 -0500

    feat: add functionality to routes, and handling errors.

--- serve/src/app/routes.py
+++ serve/src/app/routes.py
@@ -1,26 +1,66 @@
-from typing import Any
+from typing import Any, Dict, Tuple
+from fastapi import Depends, HTTPException
+from starlette.responses import RedirectResponse
 from app import app
-from app.route_model import InvocationRequest, InvocationResponse
+from app.dl_model import health_check_models
+from app.route_model import InvocationRequest, InvocationResponse, PingResponse
+from app.dl_model.image import YoloInput
 
-@app.get('/ping')
-def ping():
+
+@app.get("/", include_in_schema=False)
+def docs_redirect():
+    import os
+
+    prefix = os.getenv("CLUSTER_ROUTE_PREFIX", "").strip("/")
+    return RedirectResponse(prefix + "/docs")
+
+
+@app.get("/ping", response_model=PingResponse)
+def ping(models_check: Tuple[bool, bool] = Depends(health_check_models)):
     """Health check endpoint used by sagemaker. This needs to return 200,
-    to notify sagema that the server is all good.
+    to notify sagemaker that the server is all good.
     """
-    return {'message':'all is well'}
-
-@app.post('/invocations', response_model=InvocationResponse)
-def invocation(body: InvocationRequest) -> Any:
-    """Invocation endpoint used by sagemaker, this will execute the yolov5 model and return the detected products
-    Args:
-        body (InvocationRequest): Invocation response 
-    Returns:
-        Any: Invocation response will return the original image and the detected products
-        with information about the dimensions of the bounding boxes
-    """
-    dummy_response = {
-        'image': 'hola',
-        'detected_products': [{'product_name': 'papitas_margarita',
-                               'bounding_box': (1,2,3,4)}]
+    models_names = [
+        "YoloV5Predictor_loading_Status",
+        "ClassifierPredictor_loading_status",
+    ]
+    models_info = dict(zip(models_names, models_check))
+    if not all(models_check):
+        raise HTTPException(
+            status_code=500,
+            detail={
+                "error": "model loading error",
+                "message": "models were not loaded correctly!",
+                "models_info": models_info,
+            },
+        )
+    return {
+        "message": "health check passed: all models where already loaded.",
+        "models_info": models_info,
     }
-    return dummy_response
+
+
+@app.post("/invocations", response_model=InvocationResponse)
+def invocation(
+    body: InvocationRequest,
+    models_check: Tuple[bool, bool] = Depends(health_check_models),
+):
+    """Invocation endpoint used by sagemaker, this will execute the yolov5 and classification on an image
+    in base64 format and return the detected products.
+    """
+    try:
+        # Create yolo input object
+        yolo_input = YoloInput.from_base64_image(
+            body.base64_image, new_shape=(224, 224)
+        )
+        # Detect objects
+        return yolo_input.detect_products(conf_thres=0.1)
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail={
+                "error": str(e),
+                "message": "There was an error on the decoding of body payload or in the detections"
+                            ", check your encode base64 image is well encoded",
+            },
+        )
