cb70a2828eba87517b2cb3815bd66a834d814e35
ManuLasker <emanuel.afanador@koombea.com>
2021-05-27 12:06:05 -0500

    feat: update run local script to use docker-compose instead of docker

--- serve/scripts/run_local.sh
+++ serve/scripts/run_local.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 echo "Testing Endpoint Locally"
 
-rebuild=${1:-"rebuild"}
-default="rebuild"
-
-if [[ "$rebuild" = "$default" ]]; then
-    chmod +x scripts/*.sh
-    chmod +x src/serve
-    docker build --rm -t tienda-inference-server . -f Dockerfile
-fi
-
-docker run --rm -p 8080:8080 -v $(pwd)/models:/opt/ml/model -v $(pwd)/src:/opt/ml/code -it tienda-inference-server bash
+# run docker-compose
+docker-compose up --build
